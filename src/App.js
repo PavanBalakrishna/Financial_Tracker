6c8d62b06ca0ef7e448988bcab482ec2de4b4810
Pavan.Balakrishna <Pavan.Balakrishna@bitflyer.com>
2023-04-01 10:19:06 +0900

    Add side menu

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
   const [expensesState, setExpensesState] = useState([]);
   const [investmentsState, setInvestmentsState] = useState([]);
   const [rerenderForm, setrerenderForm] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     fetchData();
   }, []);
@@ -41,6 +42,11 @@ function App() {
     setInvestmentsState(response2);
   };
 
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Router>
       <InvestmentsContext.Provider value={{
@@ -57,80 +63,83 @@ function App() {
           }}>
             <div className="App">
               <Container fluid>
-              <nav>
-                <ul>
-                  <li>
-                    <Link to="/">Add Expense</Link>
-                  </li>
-                  <li>
-                    <Link to="/Summary">Expenses Summary</Link>
-                  </li>
-                  <li>
-                    <Link to="/All">All Expenses</Link>
-                  </li>
-                  <li>
-                    <Link to="/Investments">Investments</Link>
-                  </li>
-                  <li>
-                    <Link to="/AddInvestment">Add Investment</Link>
-                  </li>
-                  <li>
-                    <Link to="/Report">Investment Reports</Link>
-                  </li>
-                </ul>
-              </nav>
+              <button onClick={handleToggle} className="toggle-button">Menu</button>
+                <div className={`side-menu sidebar-${isOpen ? 'shown' : 'hidden'}`}>
+                <button onClick={handleToggle} className="toggle-button">Close</button>
+                  <ul>
+                    <li>
+                      <Link to="/">Add Expense</Link>
+                    </li>
+                    <li>
+                      <Link to="/Summary">Expenses Summary</Link>
+                    </li>
+                    <li>
+                      <Link to="/All">All Expenses</Link>
+                    </li>
+                    <li>
+                      <Link to="/Investments">Investments</Link>
+                    </li>
+                    <li>
+                      <Link to="/AddInvestment">Add Investment</Link>
+                    </li>
+                    <li>
+                      <Link to="/Report">Investment Reports</Link>
+                    </li>
+                  </ul>
+                </div>
+
               </Container>
               <Container className='main-content' fluid>
-              <Routes>
-                <Route path="/" exact element={<Container className="expense-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <AddExpense />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-                <Route path="/Summary" element={<Container className="summary-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <Summary />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-                <Route path="/All" exact element={<Container className="all-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <AllExpenses />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-                <Route path="/Investments" element={<Container className="investments-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <Investments />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-                <Route path="/AddInvestment" element={<Container className="addinvestment-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <AddInvestment />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-                <Route path="/Report" element={<Container className="report-container component-container" fluid>
-                  <Row>
-                    <Col md={12}>
-                      <Report />
-                    </Col>
-                  </Row>
-                </Container>}>
-                </Route>
-              </Routes>
+                <Routes>
+                  <Route path="/" exact element={<Container className="expense-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <AddExpense />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                  <Route path="/Summary" element={<Container className="summary-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <Summary />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                  <Route path="/All" exact element={<Container className="all-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <AllExpenses />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                  <Route path="/Investments" element={<Container className="investments-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <Investments />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                  <Route path="/AddInvestment" element={<Container className="addinvestment-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <AddInvestment />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                  <Route path="/Report" element={<Container className="report-container component-container" fluid>
+                    <Row>
+                      <Col md={12}>
+                        <Report />
+                      </Col>
+                    </Row>
+                  </Container>}>
+                  </Route>
+                </Routes>
               </Container>
             </div>
           </ReRenderContext.Provider>
